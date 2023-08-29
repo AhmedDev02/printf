7a3396a341ea483387037ec0ffb27455097058fd
AhmedDev02 <99210811+AhmedDev02@users.noreply.github.com>
2023-08-29 05:54:05 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,66 +1,65 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void storeToBufferAndFlush(char buffer[], int *buff_index);
 
 /**
- * _printf - Printf function
+ * custom_printf - Custom Printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: Number of printed characters.
  */
-int _printf(const char *format, ...)
+int custom_printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+	int i, printed = 0, total_printed = 0;
+	int flags, width, precision, size, buff_index = 0;
+	va_list args;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(args, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			buffer[buff_index++] = format[i];
+			if (buff_index == BUFF_SIZE)
+				storeToBufferAndFlush(buffer, &buff_index);
+			printed++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			storeToBufferAndFlush(buffer, &buff_index);
+			flags = getCustomFlags(format, &i);
+			width = getCustomWidth(format, &i, args);
+			precision = getCustomPrecision(format, &i, args);
+			size = getCustomSize(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handleCustomPrint(format, &i, args, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			printed_chars += printed;
+			total_printed += printed;
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	storeToBufferAndFlush(buffer, &buff_index);
 
-	va_end(list);
+	va_end(args);
 
-	return (printed_chars);
+	return (total_printed);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * storeToBufferAndFlush - Stores contents to the buffer and flushes if needed
+ * @buffer: Array of characters
+ * @buff_index: Index at which to add the next character, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void storeToBufferAndFlush(char buffer[], int *buff_index)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	if (*buff_index > 0)
+		write(1, &buffer[0], *buff_index);
 
-	*buff_ind = 0;
+	*buff_index = 0;
 }
